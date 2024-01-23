aba1541948d8ccb854e9063bf04138d29e91142a
tekimbaev1 <tekimbaev@gmail.com>
2024-01-23 19:38:14 +0600

    sdakf;lsdkf;sdf

--- bin/asdasdasd.dart
+++ bin/asdasdasd.dart
@@ -1,5 +1,5 @@
 import 'package:asdasdasd/asdasdasd.dart' as asdasdasd;
 
 void main(List<String> arguments) {
-  print('Hello world: ${asdasdasd.calculate()}!');
+  // print('Hello world: ${asdasdasd.calculate()}!');
 }
